47012d5c976d40d455e891c73069294e33d12466
Alexandr <hookahdelivery31@yandex.ru>
2024-01-15 01:37:45 +0300

    Начало проекта, первые шаги

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import json
 import requests
 from bs4 import BeautifulSoup
 
+
 def get_first_news():
     headers = {
         "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:109.0) Gecko/20100101 Firefox/117.0"
@@ -15,27 +16,31 @@ def get_first_news():
 
     news_dict = {}
     for article in articles_card:
-        article_title = article.find("a", class_="item-title").text.strip()
+        article_title = article.find(
+            "div", class_="item-all-title").text.strip()
         article_desc = article.find("div", class_="article").text.strip()
         article_price = article.find("div", class_="item-price").text.strip()
-        article_url = f'https://keramix-oskol.ru/catalog/tekhnopleks/{article.get("href")}'
-        
-        article_id = article_desc[11:]
+        all_a = article.find_all("a", class_="item-title")
+        for item in all_a:
+            item_text = item.text.strip()
+            item_href = "https://keramix-oskol.ru" + item.get("href")
 
-        # print(f"{article_title} | {article_desc} | {article_price} | {article_url}")
+        article_id = item_href.split("/")[-2]
 
         news_dict[article_id] = {
             "article_title": article_title,
             "article_desc": article_desc,
             "article_price": article_price,
-            "article_url": article_url
+            "article_url": item_href
         }
 
     with open("news_dict.json", "w") as file:
         json.dump(news_dict, file, indent=4, ensure_ascii=False)
 
+
 def main():
     get_first_news()
+
 
 if __name__ == '__main__':
     main()
--- test.py
+++ test.py
@@ -1,5 +1,5 @@
-url = "https://keramix-oskol.ru/catalog/tekhnopleks/tekhnopleks_1180_580_100_0_2737m3_2_7376m2_4_plity"
+url = "https://keramix-oskol.ru/catalog/tekhnopleks/tekhnopleks_1180_580_30l_nekonditsiya_1/"
 article_desc = "Код товара 0018300"
-article_id = url.split("/")[-1]
-article_id = article_desc[11:]
+article_id = url.split("/")[-2]
+article_ids = article_desc[11:]
 print(article_id)
